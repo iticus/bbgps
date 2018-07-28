f86d2e9190bb9e8ec1558001372088099f69dbd8
iticus <ticus.ionut@gmail.com>
2018-07-28 13:46:28 +0300

    cleaned up and improved code based on pylint recommendations

--- bbgps.py
+++ bbgps.py
@@ -1,101 +1,128 @@
-'''
+"""
 Created on Jul 31, 2014
 
 @author: ionut
-'''
+"""
 
 import datetime
 import json
 import logging
-import settings
-import pymongo
 import tornado.ioloop
 import tornado.web
+import pymongo
+
+import settings
 from utils import gps_to_json
 
-logging.basicConfig(level=settings.LOG_LEVEL, #filename='bbgps.log', 
-    format='[%(asctime)s] - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(level=settings.LOG_LEVEL, datefmt="%Y-%m-%d %H:%M:%S",
+                    format="[%(asctime)s] - %(levelname)s - %(message)s")
 
-_cache = {}
 
 class BaseHandler(tornado.web.RequestHandler):
-    
+    """
+    Main handler to be be inherited by subsequent handlers
+    """
+
+
     def initialize(self):
-        self.database = self.settings['database']
-        self.secret = self.settings['secret']
-    
+        self.database = self.settings["database"]
+        self.secret = self.settings["secret"]
+
+
     @tornado.web.asynchronous
     def get(self):
-        self.render('home.html')
-    
+        self.render("home.html")
+
+
     def post(self):
-        self.finish('POST not allowed') 
+        self.finish("POST not allowed")
 
 
 class UploadHandler(BaseHandler):
-    
+    """
+    Upload handler to receive and process data from bbgps application
+    """
+
+
     @tornado.web.asynchronous
     def post(self):
         data = self.request.arguments
         if not data:
             return self.finish("include position data")
-        
-        if data['secret'] != self.secret:
-            return self.finish('invalid authentication')
-            
+
+        if data["secret"] != self.secret:
+            return self.finish("invalid authentication")
+
         position = {}
-        position['bearing'] = float(data['bearing'][0])
-        position['battlevel'] = int(data['battlevel'][0])
-        position['altitude'] = float(data['altitude'][0])
-        position['provider'] = data['provider'][0]
-        position['time'] = datetime.datetime.strptime(data['time'][0], '%Y-%m-%dT%H:%M:%S.%fZ')
-        position['latitude'] = float(data['latitude'][0])
-        position['longitude'] = float(data['longitude'][0])
-        position['speed'] = float(data['speed'][0])
-        position['charging'] = int(data['charging'][0])
-        position['accuracy'] = float(data['accuracy'][0])
-        
-        _cache['last_pos'] = [position]
+        position["bearing"] = float(data["bearing"][0])
+        position["battlevel"] = int(data["battlevel"][0])
+        position["altitude"] = float(data["altitude"][0])
+        position["provider"] = data["provider"][0]
+        position["time"] = datetime.datetime.strptime(data["time"][0], "%Y-%m-%dT%H:%M:%S.%fZ")
+        position["latitude"] = float(data["latitude"][0])
+        position["longitude"] = float(data["longitude"][0])
+        position["speed"] = float(data["speed"][0])
+        position["charging"] = int(data["charging"][0])
+        position["accuracy"] = float(data["accuracy"][0])
+
+        self.application.cache["last_pos"] = [position]
         self.database.positions.insert(position)
-        logging.info('received %s' % json.dumps(position))
-        self.finish(json.dumps({'status': 'OK'}))
-    
+        logging.info("received %s", json.dumps(position))
+        self.finish(json.dumps({"status": "OK"}))
+
+
     def get(self):
-        self.finish('GET not allowed')
+        self.finish("GET not allowed")
 
 
 class WebHandler(BaseHandler):
-    
+    """
+    Retrieve position data (live or history)
+    """
+
+
     @tornado.web.asynchronous
     def get(self, op):
-        if op == 'history':
-            positions = list(self.database.positions.find({}, {"_id" : 0}).sort([('_id', 1)]).limit(8192))
-        elif op == 'now':
-            if not _cache.get('last_pos'):
-                _cache['last_pos'] = list(self.database.positions.find({}, {'_id' : 0}).sort([('_id', -1)]).limit(1))
-            positions = _cache['last_pos']
+        if op == "history":
+            pos = self.database.positions.find({}, {"_id" : 0}).sort([("_id", 1)]).limit(8192)
+        elif op == "now":
+            if not self.application.cache.get("last_pos"):
+                pos = self.database.positions.find({}, {"_id" : 0}).sort([("_id", -1)]).limit(1)
+                self.application.cache["last_pos"] = list(pos)
+            pos = self.application.cache["last_pos"]
         else:
-            positions = []
-        data = gps_to_json(positions)
+            pos = []
+        data = gps_to_json(list(pos))
         self.finish(data)
-    
+
+
     def post(self):
-        self.finish('POST not allowed')
+        self.finish("POST not allowed")
+
+
+def main():
+    """
+    Create database connection, Tornado application instance and start ioloop
+    """
+    conn = pymongo.MongoClient(settings.DB_URI)
+    database = conn[settings.DB_NAME]
+
+    application = tornado.web.Application(
+        [
+            (r"/", BaseHandler),
+            (r"/upload", UploadHandler),
+            (r"/web/([^/]+)", WebHandler),
+        ],
+        database=database, secret=settings.SECRET, debug=settings.DEBUG, gzip=True,
+        template_path=settings.TEMPLATE_PATH,
+        static_path=settings.STATIC_PATH
+    )
+    application.cache = {}
+
+    logging.info("starting bbgps...")
+    application.listen(settings.PORT)
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
-    
-    conn = pymongo.MongoClient(settings.DB_URI)
-    db = conn[settings.DB_NAME]
-    
-    application = tornado.web.Application([
-        (r"/", BaseHandler),
-        (r"/upload", UploadHandler),
-        (r"/web/([^/]+)", WebHandler),
-    ], database=db, secret=settings.SECRET, debug=settings.DEBUG, gzip=True,
-    template_path=settings.TEMPLATE_PATH,
-    static_path=settings.STATIC_PATH)
-    
-    logging.info('starting bbgps...')
-    application.listen(settings.PORT)
-    tornado.ioloop.IOLoop.instance().start()
+    main()
--- utils.py
+++ utils.py
@@ -1,28 +1,49 @@
-'''
+"""
 Created on Oct 24, 2015
 
 @author: ionut
-'''
+"""
 
 import json
 
+
 def gps_to_json(positions):
+    """
+    Transform GPS data to web format (time data to seconds since epoch)
+    :param positions: position data received from bbgps
+    :return: json data to return on request
+    """
     data = []
-    for p in positions:
-        aux = p.copy()
-        aux['time'] = int(aux['time'].strftime('%s'))
+    for position in positions:
+        aux = position.copy()
+        aux["time"] = int(aux["time"].strftime("%s"))
         data.append(aux)
     return json.dumps(data)
 
-def convert_projection(x, y, src_proj='+init=EPSG:4326', dst_proj='+init=EPSG:31700'):
-    x = float(x)
-    y = float(y)
-    import pyproj
-    x1, y1 = pyproj.transform(pyproj.Proj(src_proj), pyproj.Proj(dst_proj), x, y)
-    return x1, y1
 
-if __name__ == '__main__':
-    x = 25.0
-    y = 46.0
-    x1, y1 = convert_projection(x, y)
-    print('input [%.5f, %.5f], output [%.5f, %.5f]' % (x, y, x1, y1))
+def convert_projection(lng, lat, src_proj="+init=EPSG:4326", dst_proj="+init=EPSG:31700"):
+    """
+    Convert coordinates from src_proj to dst_proj
+    :param lng: longitude
+    :param lat: latitude
+    :param src_proj: source projection
+    :param dst_proj: destination projection
+    """
+    lng = float(lng)
+    lat = float(lat)
+    import pyproj
+    return pyproj.transform(pyproj.Proj(src_proj), pyproj.Proj(dst_proj), lng, lat)
+
+
+def main():
+    """
+    Test coordinate transform function with sample data
+    """
+    longitude = 25.0
+    latitude = 46.0
+    longitude1, latitude1 = convert_projection(longitude, latitude)
+    print("input [%.5f, %.5f], output [%.5f, %.5f]" % (longitude, latitude, longitude1, latitude1))
+
+
+if __name__ == "__main__":
+    main()
